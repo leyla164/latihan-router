1883b45925d9abb5281441109e871045c8621798
leyla164 <leylamaisarahdasni@gmail.com>
2023-08-02 15:01:54 +0700

    latihan-router

---
+++ src/views/Detail2.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Detail {{ detail2.nama }}</h1>
+    <h4>Harga : {{ detail2.harga }}</h4>
+</template>
+<script>
+   import { kategori } from '@/assets/Kategori.js';
+
+   export default {
+    props: [
+        "id_kategori"
+    ],
+    setup(props)
+    {
+       const detail2 = kategori["kategori"].find(function(item) {
+         return item.id == props.id_kategori;
+        });
+        return {
+         detail2
+       }
+     } 
+   }
+</script>
